fef817eeaea57ddac97fe3c0bdd103a9789ede76
Ivan Immanuel <ivanimmanuel1234@gmail.com>
2026-03-12 17:07:53 -0400

    Implemented playGame function.

--- A3/Game/game.c
+++ A3/Game/game.c
@@ -1,9 +1,27 @@
-// Game.cpp : This file contains the 'main' function.
 #include "game.h"
 
 const char* playGame(const char* player1Shape, const char* player2Shape)
 {
-    return 0;
+	if (player1Shape == STR_ROCK && player2Shape == STR_ROCK)
+		return STR_DRAW;
+	else if (player1Shape == STR_ROCK && player2Shape == STR_PAPER)
+		return STR_PLAYER2;
+	else if (player1Shape == STR_ROCK && player2Shape == STR_SCISSORS)
+		return STR_PLAYER1;
+	else if (player1Shape == STR_PAPER && player2Shape == STR_ROCK)
+		return STR_PLAYER1;
+	else if (player1Shape == STR_PAPER && player2Shape == STR_PAPER)
+		return STR_DRAW;
+	else if (player1Shape == STR_PAPER && player2Shape == STR_SCISSORS)
+		return STR_PLAYER2;
+	else if (player1Shape == STR_SCISSORS && player2Shape == STR_ROCK)
+		return STR_PLAYER2;
+	else if (player1Shape == STR_SCISSORS && player2Shape == STR_PAPER)
+		return STR_PLAYER1;
+	else if (player1Shape == STR_SCISSORS && player2Shape == STR_SCISSORS)
+		return STR_DRAW;
+	else
+		return STR_INVALID;
 }
 
 int main()
