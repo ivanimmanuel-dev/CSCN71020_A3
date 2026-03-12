86315f32897bc94eb0b1c0b36aaf0fd422b127a0
Ivan Immanuel <ivanimmanuel1234@gmail.com>
2026-03-12 16:33:48 -0400

    Minor errors fixed and code cleaned up

--- A3/Game/game.c
+++ A3/Game/game.c
@@ -1,12 +1,9 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Game.cpp : This file contains the 'main' function.
 #include "game.h"
 
 const char* playGame(const char* player1Shape, const char* player2Shape)
 {
-	//#write functional code here
-	return STR_INVALID;
+
 }
 
 int main()
--- A3/Game/game.h
+++ A3/Game/game.h
@@ -1,24 +1,20 @@
 #pragma once
-
-#pragma once
 #include <stdbool.h>
 
-/*Rock, paper, scissor Hand shapes definition*/
 #define STR_ROCK	"Rock"
 #define STR_PAPER	"Paper"
-#define STR_SCISSORS	"Scissor"
+#define STR_SCISSORS	"Scissors"
 
-/* game results definition */
 #define STR_INVALID	"Invalid"
 #define STR_DRAW	"Draw"
 #define STR_PLAYER1	"Player1"
 #define STR_PLAYER2	"Player2"
 
 #if defined(__cplusplus)
-extern "C" {
+extern "C" 
+{
 #endif
 
-	/* Function prototypes .c */
 	const char* playGame(const char* player1Shape, const char* player2Shape);
 
 #if defined(__cplusplus)
